89311ef6c9532587e4c7f5b8278b2d1d0067ddf2
ravenaidy <alistair1ockhuis@gmail.com>
2022-02-28 11:26:02 +0200

    Untracked files

---
+++ src/MediatorApi/Dependencies/MediatorServices.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using Mediator.Core.Account.Contracts;
+using Mediator.Infrastructure.Connections;
+using Mediator.Infrastructure.Connections.Contracts;
+using Mediator.Infrastructure.Repositories.Account;
+using Mediator.Infrastructure.Repositories.Cache.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CouchbaseCache = Mediator.Infrastructure.Repositories.Cache.CouchbaseCache;
+
+namespace Mediator.Api.Dependencies
+{
+    public static class MediatorServices
+    {
+        public static IServiceCollection AddMediatorServices(this IServiceCollection services, IConfiguration configuration)
+        {            
+            services.AddSingleton<IConnectionFactory>(sp =>
+                new ConnectionFactory<SqlConnection>(configuration.GetConnectionString("Mediator")));
+            services.AddSingleton<IAccountRepository, AccountRepository>();
+            services.AddSingleton<ICache, CouchbaseCache>();
+            return services;
+        }
+    }
+}
